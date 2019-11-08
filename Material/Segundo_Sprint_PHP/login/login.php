a20ebbe08d9b61542bb9402dbaae1636f0e27969
unknown <ignaciocivetta@gmail.com>
2019-11-08 15:32:42 -0300

    Pasados a PHP 	new file:   Material/Segundo_Sprint_PHP/Home/img/asd.jpg 	new file:   Material/Segundo_Sprint_PHP/Home/img/bg-body.jpg 	new file:   Material/Segundo_Sprint_PHP/Home/img/bg3.png 	new file:   Material/Segundo_Sprint_PHP/Home/img/carro1.jpg 	new file:   Material/Segundo_Sprint_PHP/Home/img/carro2.jpg 	new file:   Material/Segundo_Sprint_PHP/Home/img/carro3.jpg 	new file:   Material/Segundo_Sprint_PHP/Home/img/dog1.jfif 	new file:   Material/Segundo_Sprint_PHP/Home/img/logo.png 	new file:   Material/Segundo_Sprint_PHP/Home/img/logo2.png 	new file:   Material/Segundo_Sprint_PHP/Home/img/perro1.jpg 	new file:   Material/Segundo_Sprint_PHP/Home/img/perro1wide.jpg 	new file:   Material/Segundo_Sprint_PHP/Home/img/perro2.jpg 	new file:   Material/Segundo_Sprint_PHP/Home/img/perro2wide.jpg 	new file:   Material/Segundo_Sprint_PHP/Home/img/perro3.jpg 	new file:   Material/Segundo_Sprint_PHP/Home/img/perro3wide.jpg 	new file:   Material/Segundo_Sprint_PHP/Home/img/perro4wide.jpg 	new file:   Material/Segundo_Sprint_PHP/Home/img/perro5wide.jpg 	new file:   Material/Segundo_Sprint_PHP/Home/img/perro6wide.jpg 	new file:   Material/Segundo_Sprint_PHP/Home/index.php 	new file:   Material/Segundo_Sprint_PHP/Home/post.css 	new file:   Material/Segundo_Sprint_PHP/Home/post.html 	new file:   Material/Segundo_Sprint_PHP/Home/style.css 	new file:   Material/Segundo_Sprint_PHP/botones rs footer/index.html 	new file:   Material/Segundo_Sprint_PHP/botones rs footer/style.css 	new file:   Material/Segundo_Sprint_PHP/contacto/contacto.css 	new file:   Material/Segundo_Sprint_PHP/contacto/contacto.php 	new file:   Material/Segundo_Sprint_PHP/contacto/imgs/bg-body.jpg 	new file:   Material/Segundo_Sprint_PHP/contacto/imgs/bg.png 	new file:   Material/Segundo_Sprint_PHP/contacto/imgs/bg2.png 	new file:   Material/Segundo_Sprint_PHP/contacto/imgs/bg3.png 	new file:   Material/Segundo_Sprint_PHP/contacto/imgs/logo.png 	new file:   Material/Segundo_Sprint_PHP/contacto/imgs/logo2.png 	new file:   Material/Segundo_Sprint_PHP/faq/faq.css 	new file:   Material/Segundo_Sprint_PHP/faq/faq.php 	new file:   Material/Segundo_Sprint_PHP/faq/img/bg-body.jpg 	new file:   Material/Segundo_Sprint_PHP/faq/img/logo.png 	new file:   Material/Segundo_Sprint_PHP/faq/img/logo2.png 	new file:   Material/Segundo_Sprint_PHP/header.css 	new file:   Material/Segundo_Sprint_PHP/login/estilos.css 	new file:   Material/Segundo_Sprint_PHP/login/img/bg.png 	new file:   Material/Segundo_Sprint_PHP/login/img/bg2.png 	new file:   Material/Segundo_Sprint_PHP/login/img/dog1.jfif 	new file:   Material/Segundo_Sprint_PHP/login/img/dog2.jfif 	new file:   Material/Segundo_Sprint_PHP/login/img/dog2.jpg 	new file:   Material/Segundo_Sprint_PHP/login/img/dog2.png 	new file:   Material/Segundo_Sprint_PHP/login/img/dog3.jpg 	new file:   Material/Segundo_Sprint_PHP/login/img/logo.png 	new file:   Material/Segundo_Sprint_PHP/login/img/logo2.png 	new file:   Material/Segundo_Sprint_PHP/login/login.php 	new file:   Material/Segundo_Sprint_PHP/registro/img/bg.png 	new file:   Material/Segundo_Sprint_PHP/registro/img/bg2.png 	new file:   Material/Segundo_Sprint_PHP/registro/img/dog1.jfif 	new file:   Material/Segundo_Sprint_PHP/registro/img/dog2.jfif 	new file:   Material/Segundo_Sprint_PHP/registro/img/dog2.jpg 	new file:   Material/Segundo_Sprint_PHP/registro/img/dog2.png 	new file:   Material/Segundo_Sprint_PHP/registro/img/dog3.jpg 	new file:   Material/Segundo_Sprint_PHP/registro/img/logo.png 	new file:   Material/Segundo_Sprint_PHP/registro/img/logo2.png 	new file:   Material/Segundo_Sprint_PHP/registro/registro.css 	new file:   Material/Segundo_Sprint_PHP/registro/registro.php i pasado a php
    php

---
+++ Material/Segundo_Sprint_PHP/login/login.php
@@ -0,0 +1,93 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <link rel="stylesheet" href="estilos.css">
+  <title>Inicia Sesión</title>
+</head>
+<body>
+    <header>
+        <div class="logo">
+          <a href="../home/index.html">
+            <img src="img/logo2.png" alt="logo">
+          </a>
+        </div>
+        <div class="buscador">
+          <select name="Barrio">
+              <option value="Barrio"disabled selected>-Selecciona tu barrio-</option>
+              <option value="Almagro">Almagro</option>
+              <option value="Balvanera">Balvanera</option>
+              <option value="Belgrano">Belgrano</option>
+              <option value="Colegiales">Colegiales</option>
+              <option value="Chacarita">Chacarita</option>
+              <option value="Coghlan">Coghlan</option>
+              <option value="Caballito">Caballito</option>
+              <option value="Flores">Flores</option>
+              <option value="Nueva Pompeya">Nueva Pompeya</option>
+              <option value="Palermo">Palermo</option>
+              <option value="San Telmo">San Telmo</option>
+              <option value="Versalles">Versalles</option>
+              <option value="Villa Luro">Villa Luro</option>
+          </select>
+        </div>
+        <div class="preguntas">
+          <a href="../faq/faq.html">Preguntas Frecuentes</a>
+        </div>
+        </div>
+    </header>
+<main>
+
+
+    <div class="container" id="container">
+        <div class="form-container sign-up-container">
+          <form action="#">
+            <h1>Create Account</h1>
+            <div class="social-container">
+              <a href="#" class="social"><i class="fab fa-facebook-f"></i></a>
+              <a href="#" class="social"><i class="fab fa-google-plus-g"></i></a>
+              <a href="#" class="social"><i class="fab fa-linkedin-in"></i></a>
+            </div>
+            <span>or use your email for registration</span>
+            <input type="text" placeholder="Name" />
+            <input type="email" placeholder="Email" />
+            <input type="password" placeholder="Password" />
+            <button>Sign Up</button>
+          </form>
+        </div>
+        <div class="form-container sign-in-container">
+          <form action="#">
+            <h1>Inicia Sesion</h1>
+            <div class="social-container">
+              <a href="#" class="social"><i class="fab fa-facebook-f"></i></a>
+              <a href="#" class="social"><i class="fab fa-google-plus-g"></i></a>
+              <a href="#" class="social"><i class="fab fa-linkedin-in"></i></a>
+            </div>
+            <span>O usa tu cuenta</span>
+            <input type="email" placeholder="Email" />
+            <input type="password" placeholder="Contraseña" />
+            <a href="#">Olvidaste tu contraseña?</a>
+            <button>Iniciar Sesión</button>
+          </form>
+        </div>
+        <div class="overlay-container">
+            <div class="overlay">
+                <div class="overlay-panel overlay-left">
+                  <h1>Bienvenido!</h1>
+                  <p>Para seguir conectado, por favor inicia sesión con tus datos</p>
+                  <button class="ghost" id="signIn">Sign In</button>
+                </div>
+                <div class="overlay-panel overlay-right">
+                  <h1>Hola, Pet-amigo!</h1>
+                  <p>Ingresa tus datos personales y comienza una aventura con nosotros</p>
+                  <a href="../registro2.0/registro.html"><button class="ghost" id="signUp">Registrate</button></a>
+                </div>
+            </div>
+        </div>
+    </div
+  </main>
+
+
+</body>
+</html>
